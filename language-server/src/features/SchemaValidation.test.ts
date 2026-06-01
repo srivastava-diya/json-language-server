f7f0d08e49ba08e411486119d09e90c04d3fc0ae
Diya <diyasrivastava2023@gmail.com>
2026-06-01 23:15:05 +0530

    refactor: add Diagnostic provider to SchemaValidation

--- language-server/src/features/SchemaValidation.test.ts
+++ language-server/src/features/SchemaValidation.test.ts
@@ -1,9 +1,9 @@
 import { describe, test, expect, beforeAll, afterAll, afterEach } from "vitest";
 import { TextDocumentSyncKind } from "vscode-languageserver";
-import { TestClient } from "./test/test-client.ts";
+import { TestClient } from "../test/test-client.ts";
 import { registerSchema, unregisterSchema } from "@hyperjump/json-schema";
 
-describe("JSON Language Server", () => {
+describe("Schema Validation", () => {
   let client: TestClient;
   const fixtureSchemaUri = "https://example.com/person";
 
@@ -22,34 +22,6 @@ describe("JSON Language Server", () => {
 
   test("textDocumentSync = Incremental", () => {
     expect(client.serverCapabilities?.textDocumentSync).to.equal(TextDocumentSyncKind.Incremental);
-  });
-
-  test("invalid JSON syntax returns a diagnostic", async () => {
-    const diagnosticsPromise = new Promise((resolve) => {
-      client.onNotification("textDocument/publishDiagnostics", (params) => {
-        resolve(params.diagnostics);
-      });
-    });
-
-    await client.writeDocument("test.json", `{ "name": }`);
-    await client.openDocument("test.json");
-
-    const diagnostics = await diagnosticsPromise;
-    expect(diagnostics).toHaveLength(1);
-  });
-
-  test("valid JSON Syntax should not return a diagnostic", async () => {
-    const diagnosticsPromise = new Promise((resolve) => {
-      client.onNotification("textDocument/publishDiagnostics", (params) => {
-        resolve(params.diagnostics);
-      });
-    });
-
-    await client.writeDocument("test.json", `{ "Name": "Foo" }`);
-    await client.openDocument("test.json");
-
-    const diagnostics = await diagnosticsPromise;
-    expect(diagnostics).toHaveLength(0);
   });
 
   test("JSON Validation using Hyperjump - Valid Case", async () => {
@@ -71,10 +43,10 @@ describe("JSON Language Server", () => {
     registerSchema(testSchema, fixtureSchemaUri);
 
     await client.writeDocument("instance.json", `{
-    "$schema": "${fixtureSchemaUri}",
-    "name": "Alice",
-    "age" : 39
-    }`);
+        "$schema": "${fixtureSchemaUri}",
+        "name": "Alice",
+        "age" : 39
+        }`);
     await client.openDocument("instance.json");
 
     const diagnostics = await diagnosticsPromise;
@@ -196,7 +168,7 @@ describe("JSON Language Server", () => {
       $schema: "https://json-schema.org/draft/2020-12/schema",
       type: "object",
       properties: {
-        "foo/bar" : {type : "string"}
+        "foo/bar": { type: "string" }
       }
     };
 
---
+++ language-server/src/features/SchemaValidation.ts
@@ -0,0 +1,67 @@
+import { Diagnostic, DiagnosticSeverity } from "vscode-languageserver";
+import { TextDocument } from "vscode-languageserver-textdocument";
+import * as jsonc from "jsonc-parser";
+import { validate } from "@hyperjump/json-schema-errors";
+import { pointerSegments } from "@hyperjump/json-pointer"
+
+import type { ErrorObject } from "@hyperjump/json-schema-errors";
+import type { DiagnosticsProvider } from "./Diagnostics.ts";
+
+export class SchemaValidation implements DiagnosticsProvider {
+  async getDiagnostics(textDocument: TextDocument) {
+    const text = textDocument.getText();
+    const parseErrors: jsonc.ParseError[] = [];
+
+    const tree = jsonc.parseTree(text, parseErrors);
+    const schemaDiagnostics: Diagnostic[] = [];
+
+    const schemaNode = tree ? jsonc.findNodeAtLocation(tree, ["$schema"]) : undefined;
+    const schemaUri = schemaNode?.value;
+
+    // skip schema validation if there are syntax errors hence the parseError.length check
+    if (schemaUri && parseErrors.length === 0) {
+      let instance = JSON.parse(text);
+      const result = await validate(schemaUri, instance);
+
+      if (!result.valid) {
+        const errors = result.errors;
+        errors.forEach((error) => {
+          const node = tree ? findNodeByPointer(tree, error.instanceLocation) : undefined;
+
+          if (node) {
+            schemaDiagnostics.push({
+              severity: DiagnosticSeverity.Error,
+              range: {
+                start: textDocument.positionAt(node.offset),
+                end: textDocument.positionAt(node.offset + node.length)
+              },
+              message: formatError(error),
+              source: "json-language-server"
+            });
+          }
+        });
+      }
+    }
+
+    return schemaDiagnostics;
+  }
+}
+
+const findNodeByPointer = (node: jsonc.Node, pointer: string) => {
+  if(pointer === "#"){
+    return node;
+  }
+  const segments = [...pointerSegments(pointer.slice(1))];
+  const path = segments.map(s => /^\d+$/.test(s) ? parseInt(s) : s);
+  return jsonc.findNodeAtLocation(node, path)
+};
+
+const formatError = (error: ErrorObject, depth = 0): string => {
+  let msg = error.message;
+  if (error.alternatives && error.alternatives.length > 0) {
+    const indent = "  ".repeat(depth + 1);
+    const lines = error.alternatives.flatMap((alt) => alt.map((subErr) => `${indent}- ${formatError(subErr, depth + 1)}`));
+    msg += `:\n${lines.join("\n")}`;
+  }
+  return msg;
+};
